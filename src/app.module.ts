07a7e8427ce31e8853607a3acb1aa34699e2c493
milad <miladbachari14@gmail.com>
2023-06-19 21:19:18 +0330

    init nest-auth

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ".env",
+      isGlobal:true,
+    }),
+    AuthModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABACE_URL,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { setupSwagger } from './setup-swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+  }));
+
+  if (!(process.env.NODE_ENV === 'production')) {
+    setupSwagger(app);
+  }
+  await app.listen(3000);
+}
+bootstrap();
